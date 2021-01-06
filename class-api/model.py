5f917c95f7f7c2a7e18c2731e04d68db5877a0a1
Clem2798 <clement.ribes@imt-atlantique.net>
2021-01-06 16:56:51 +0100

    Correction of the model prediction (resolution of a name conflict)

--- class-api/model.py
+++ class-api/model.py
@@ -1,14 +1,13 @@
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.resnet import preprocess_input, decode_predictions, ResNet50
-from keras.applications.resnet import decode_predictions
-from keras.applications.nasnet import preprocess_input, decode_predictions, NASNetMobile
+
 
 def classify(url):
     image = load_img("./" + url, target_size=(224, 224))
     image = img_to_array(image)
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    label =  decode_predictions(ResNet50().predict(preprocess_input(image)))
+    label = decode_predictions(ResNet50().predict(preprocess_input(image)))
     label = label[0][0]
     return label
 
