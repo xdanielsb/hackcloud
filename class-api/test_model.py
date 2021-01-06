10b5a1e816fbb4ef92f978092b2309aff2d472cb
Clem2798 <clement.ribes@imt-atlantique.net>
2021-01-06 16:38:51 +0100

    added test code ml backend

--- class-api/test_model.py
+++ class-api/test_model.py
@@ -1,19 +1,17 @@
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.applications.resnet import preprocess_input, decode_predictions, ResNet50
-from keras.applications.resnet import decode_predictions
 from keras.applications.nasnet import preprocess_input, decode_predictions, NASNetMobile
+import pytest
 
 def classify(url):
     image = load_img("./" + url, target_size=(224, 224))
     image = img_to_array(image)
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-    label =  decode_predictions(ResNet50().predict(preprocess_input(image)))
+    label =  decode_predictions(NASNetMobile().predict(preprocess_input(image)))
     label = label[0][0]
     return label
 
-
-if __name__ == "__main__":
-    # example
-    ans = classify("balloon.jpg")
-    print(ans)
+@pytest.mark.timeout(10)
+def test_classify():
+    assert classify("balloon.jpg")[1] == "balloon"
