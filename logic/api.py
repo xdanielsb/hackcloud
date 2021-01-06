19e7c4b7f4f8abb1b8c722b425b118adf10507eb
Daniel <dfsantosbu@unal.edu.co>
2021-01-06 17:57:47 +0100

    fix: instead of b64 send actual file
    update test image

--- logic/api.py
+++ logic/api.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from io import BytesIO
+
+import cv2
+import numpy as np
+from flask import Flask, request
 from flask_cors import CORS
-
-# from model import classify
-from flask import request
-
+from model import classify
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # label = classify(url)
-    data = request.get_json()
-    print(data)
-    # return {"label": str(label)}
-    return {"ans": "hello"}
+    photo = request.files["file"]
+    in_memory_file = BytesIO()
+    photo.save(in_memory_file)
+    data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+    label = classify(cv2.imdecode(data, 1))
+    return {"label": str(label)}
