1e3e9cac672b2761652ab70d5b1780b80d35ea51
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-06 14:25:32 +0100

    solved cors issues

--- logic/api.py
+++ logic/api.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 # from model import classify
 from flask import request
 
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}})
 
 @app.route("/", methods=["GET", "POST"])
 def index():
